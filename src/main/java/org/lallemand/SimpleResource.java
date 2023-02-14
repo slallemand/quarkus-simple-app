4759279863485bdaa573479d81e9e0a291fe2cf1
Sebastien LALLEMAND <sld@redhat.com>
2023-02-14 16:08:06 +0100

    Externalization of message property

--- src/main/java/org/lallemand/SimpleResource.java
+++ src/main/java/org/lallemand/SimpleResource.java
@@ -5,16 +5,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+
 @Path("/")
 public class SimpleResource {
-
-    private String prefix = "Bonjour";
+    @ConfigProperty(name = "greeting.message")
+    private String prefix;
 
     private String HOSTNAME =
        System.getenv().getOrDefault("HOSTNAME", "unknown");
-
+    
     private int count = 0;
-
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
